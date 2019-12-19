66f98a79f064a9d60a358dda757d2b9944a6bac3
sujith-WW <59042456+sujith-WW@users.noreply.github.com>
2019-12-19 11:11:55 +0530

    Uploaded files -001

---
+++ Naukri.bat
@@ -0,0 +1,5 @@
+set projectLocation=E:\Winwire\Builds\NaukriChrome
+cd E:\Winwire\Builds\NaukriChrome
+set classpath=E:\Winwire\Builds\NaukriChrome\bin;E:\Winwire\Builds\NaukriChrome\lib\*
+org.testng.TestNG E:\Winwire\Builds\NaukriChrome\testng.xml
+pause
